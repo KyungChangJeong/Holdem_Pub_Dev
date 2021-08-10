fb19087e7fd7396652ed506dbc33f1ca09920f4e
gyungchang <rudckd135@naver.com>
2021-08-10 22:34:26 +0900

    예약자 인원 -> 현재 인원 리스트 변경 / 관리자 직접추가 버튼 추가

--- lib/view/shop_manage.dart
+++ lib/view/shop_manage.dart
@@ -1,15 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:holdem_pub/model/user.dart';
 import 'package:holdem_pub/view/shop_manage_setting.dart';
+import 'package:holdem_pub/view/shop.dart';
 
 class ShopManage extends StatefulWidget {
   const ShopManage({Key? key}) : super(key: key);
+
   @override
   _ShopManageState createState() => _ShopManageState();
 }
 
+var ReserveUser = [
+  {'nickname': '예약1', 'name': '선명', 'phonenumber': '010-2620-5991'},
+  {'nickname': '예약2', 'name': '경민', 'phonenumber': '010-1230-5991'},
+  {'nickname': '예약3', 'name': '경창', 'phonenumber': '010-8720-8661'},
+  {'nickname': '예약4', 'name': '제현', 'phonenumber': '010-215691'},
+  {'nickname': '예약5', 'name': '우석', 'phonenumber': '05405'}
+];
+var GameUser = [
+  {'nickname': '게임1', 'name': '당당', 'phonenumber': '010-1561-8661'},
+  {'nickname': '게임2', 'name': '수구리', 'phonenumber': '010-2156-1591'},
+  {'nickname': '게임3', 'name': '굳굳', 'phonenumber': '010-1150-5991'},
+];
+
 class _ShopManageState extends State<ShopManage> {
   String description = "~~~~~~~~~~";
+  Map<String, String> temp = {
+    'nickname': '직접예약',
+    'name': '알수없음',
+    'phonenumber': ''
+  };
 
+  // 바 컨트롤러 생성
+  final ScrollController _scrollController = ScrollController();
+
+  void _showMaterialDialog(Map<String, String> status, int index) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('상태 변경'),
+            // content: Text('ㅎㅇ'),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () {
+                    _dismissDialog();
+                    print('status: $status');
+                    // 예약 => 게임
+                    setState(() {
+                      // ReserveUser에서 status 삭제
+                      ReserveUser.removeAt(index);
+                      // GameUser에 status 추가
+                      GameUser.add(status);
+                    });
+                  },
+                  child: Text('수락')),
+              TextButton(
+                onPressed: () {
+                  _dismissDialog();
+                },
+                child: Text('취소'),
+              )
+            ],
+          );
+        });
+  }
+
+  _dismissDialog() {
+    Navigator.pop(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,124 +76,198 @@ class _ShopManageState extends State<ShopManage> {
         appBar: AppBar(
           title: Text('관리자 페이지'),
         ),
-        body: Column(
-          children: [
-            // 매장 이름
-            Container(
-              padding: EdgeInsets.all(20),
-              child: Text(
-                '매장 1',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-
-            // 매장 정보 변경
-            Container(
-              // width: 100,
-              // height: 100,
-              // child: Row(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: [
-              //     // SizedBox(
-              //     //     child: Image.network('https://picsum.photos/250?image=9')),
-              //     // TextFiled로 내용 수정 가능
-              //     Container(
-              //       width: 100,
-              //       height: 100,
-              //       child: TextField(
-              //         decoration: InputDecoration(
-              //           // icon: Icon(Icons.shop),
-              //           border: OutlineInputBorder(),
-              //           labelText: description,
-              //         ),
-              //       ),
-              //     )
-              //   ],
-              // ),
-            ),
-
-            // 예약인원 설정 및 현재인원 설정 버튼
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+        body: Scrollbar(
+          // <- ScrollBar에 컨트롤러를 알려준다
+          controller: _scrollController,
+          // <- 화면에 항상 스크롤바가 나오도록 한다
+          isAlwaysShown: true,
+          thickness: 15,
+          child: ListView(
+            controller: _scrollController,
+            // scrollDirection: Axis.vertical,
+            // shrinkWrap: true,
+            children: [
+              Column(
                 children: [
-                  TextButton(onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ShopManageSetting()),
-                    );
-                  }, child: Text('예약 설정')),
-
-                  // TextButton(onPressed: () {}, child: Text('현재 인원 설정')),
-                ],
-              ),
-            ),
-
-            // 예약자 현황
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 1,
-                  color: Colors.grey,
-                ),
-              ),
-
-              /// Listbuilder로 변경하기 (동적 생성)
-              // Local 데이터로 삭제 및 수락 가능하도록
-              child: ListView(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                children: [
-                  Text(
-                    '예약자 현황 \n',
-                    style: TextStyle(fontSize: 18),
+                  // 매장 이름
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      '매장 1',
+                      style: TextStyle(fontSize: 18),
+                    ),
                   ),
+
+                  // 매장 정보 변경
+                  Container(
+                      // width: 100,
+                      // height: 100,
+                      // child: Row(
+                      //   mainAxisAlignment: MainAxisAlignment.center,
+                      //   children: [
+                      //     // SizedBox(
+                      //     //     child: Image.network('https://picsum.photos/250?image=9')),
+                      //     // TextFiled로 내용 수정 가능
+                      //     Container(
+                      //       width: 100,
+                      //       height: 100,
+                      //       child: TextField(
+                      //         decoration: InputDecoration(
+                      //           // icon: Icon(Icons.shop),
+                      //           border: OutlineInputBorder(),
+                      //           labelText: description,
+                      //         ),
+                      //       ),
+                      //     )
+                      //   ],
+                      // ),
+                      ),
+
+                  // 예약인원 설정 및 현재인원 설정 버튼
                   Container(
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text('닉네임'),
-                        TextButton(onPressed: (){
-                          // 게임 대기 인원으로 내리기
-                        }, child: Text('현재인원으로 전환')),
-                        TextButton(onPressed: (){
-                          // List 삭제
-                        }, child: Text('취소')),
+                        TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ShopManageSetting()),
+                              );
+                            },
+                            child: Text('예약 설정')),
+
+                        // TextButton(onPressed: () {}, child: Text('현재 인원 설정')),
                       ],
                     ),
+                  ),
 
+                  // 예약자 현황
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+
+                    /// Listbuilder로 변경하기 (동적 생성)
+                    // Local 데이터로 삭제 및 수락 가능하도록
+                    // child: ListView(
+                    //   scrollDirection: Axis.vertical,
+                    //   shrinkWrap: true,
+                    //   children: [
+                    //     Text(
+                    //       '예약자 현황 \n',
+                    //       style: TextStyle(fontSize: 18),
+                    //     ),
+                    //     Container(
+                    //       child: Row(
+                    //         mainAxisAlignment: MainAxisAlignment.center,
+                    //         children: [
+                    //           Text('닉네임'),
+                    //           TextButton(onPressed: (){
+                    //             // 게임 대기 인원으로 내리기
+                    //           }, child: Text('현재인원으로 전환')),
+                    //           TextButton(onPressed: (){
+                    //             // List 삭제
+                    //             // print("User1 : ${User1[3]['name']}");
+                    //           }, child: Text('취소')),
+                    //         ],
+                    //       ),
+                    //
+                    //     ),
+                    //   ],
+                    // ),
+
+                    child: Column(
+                      children: [
+                        Text(
+                          '대기 인원',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        // 관리자 직접 예약자 추가
+                        IconButton(
+                            onPressed: () {
+                              setState(() {
+                                // ReserveUser
+                                ReserveUser.add(temp);
+                              });
+                            },
+                            icon: Icon(Icons.add)),
+                        Container(
+                          height: 400,
+                          child: new ListView.builder(
+                            padding: const EdgeInsets.all(8),
+                            itemCount: ReserveUser.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return new ListTile(
+                                onTap: () {
+                                  print(ReserveUser.runtimeType);
+                                  _showMaterialDialog(
+                                      ReserveUser[index], index);
+                                },
+                                leading: Icon(Icons.person),
+                                title: Text(
+                                    ReserveUser[index]['nickname'].toString()),
+                                subtitle:
+                                    Text(ReserveUser[index]['name'].toString()),
+                                trailing: Text(ReserveUser[index]['phonenumber']
+                                    .toString()),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // 게임 대기 인원 (매장 내 있는 인원)
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Text('현재인원'),
+                        // 사용자 현재인원 직접 추가
+                        IconButton(
+                            onPressed: () {
+                              setState(() {
+                                // GameUser
+                                GameUser.add(temp);
+                              });
+                            },
+                            icon: Icon(Icons.add)),
+                        Container(
+                          height: 400,
+                          child: new ListView.builder(
+                            padding: const EdgeInsets.all(8),
+                            itemCount: GameUser.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return new ListTile(
+                                leading: Icon(Icons.person),
+                                title: Text(
+                                    GameUser[index]['nickname'].toString()),
+                                subtitle:
+                                    Text(GameUser[index]['name'].toString()),
+                                trailing: Text(
+                                    GameUser[index]['phonenumber'].toString()),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
-              ),
-            ),
-
-            // 게임 대기 인원 (매장 내 있는 인원)
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 1,
-                  color: Colors.grey,
-                ),
-              ),
-              child: ListView(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                children: [
-                  Text(
-                    '게임 대기 인원 \n',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  ListTile(
-                    leading: Text('닉네임'),
-                  ),
-                  ListTile(
-                    leading: Text('닉네임'),
-                  ),
-                ],
-              ),
-            ),
-          ],
+              )
+            ],
+          ),
         ));
   }
 }
-
